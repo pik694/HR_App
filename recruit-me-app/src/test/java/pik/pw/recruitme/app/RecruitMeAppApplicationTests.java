bed605c00bc80730ec617afc1d4c4fe26b37fa95
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-03-22 20:50:26 +0100

    Add JUnit tests

--- recruit-me-app/src/test/java/pik/pw/recruitme/app/RecruitMeAppApplicationTests.java
+++ recruit-me-app/src/test/java/pik/pw/recruitme/app/RecruitMeAppApplicationTests.java
@@ -5,22 +5,21 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RecruitMeAppApplicationTests {
 
     @Autowired
     SampleController controller;
 
     @Test
-    public void contextLoads() {
-    }
+    public void someNotFailingTest() {
 
-    @Test
-    public void someFailingTest() {
         assertThat(controller).isNotNull();
+
     }
 }
