3b359029e9b8a21fd52dda3ff74fc127e677de31
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-03-23 21:52:56 +0100

    Create HTTP server script files
    Refers to #7

--- Database/database_init.sh
+++ Database/database_init.sh
@@ -4,6 +4,7 @@ DB_CONTAINER=recruit-me-db
 DB_IMAGE=recruit-me-db:0.1.0
 DB_VOLUME=recruit-me-db-volume
 
+
 docker ps -aqf "name=${DB_CONTAINER}" | xargs docker stop | xargs docker rm
 docker rmi ${DB_IMAGE}
 docker build -t ${DB_IMAGE} .
