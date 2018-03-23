b2fcec419648417ce7f7ec6eb314bd5c6e07208f
Daniel Bigos <daniel.bigos96@gmail.com>
2018-03-23 19:13:32 +0100

    Modify database scripts
    Refers to #10

--- Database/database_init.sh
+++ Database/database_init.sh
@@ -8,5 +8,5 @@ docker ps -aqf "name=${DB_CONTAINER}" | xargs docker stop | xargs docker rm
 docker rmi ${DB_IMAGE}
 docker build -t ${DB_IMAGE} .
 
-docker run --name=${DB_CONTAINER} -p 3306:3306 -d -v ${DB_VOLUME}:/dupa ${DB_IMAGE}
+docker run --name=${DB_CONTAINER} -p 3306:3306 -d ${DB_IMAGE}
 
