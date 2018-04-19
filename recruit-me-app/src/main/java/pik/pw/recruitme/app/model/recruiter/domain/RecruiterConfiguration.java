bdd1ab04e7c2f5b5902fa0d6e0e4847968e9e490
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-04-19 15:53:35 +0200

    Make use of profiles
    Refers to #28, #41

--- recruit-me-app/src/main/java/pik/pw/recruitme/app/model/recruiter/domain/RecruiterConfiguration.java
+++ recruit-me-app/src/main/java/pik/pw/recruitme/app/model/recruiter/domain/RecruiterConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 class RecruiterConfiguration {
 
-
     RecruiterFacade recruiterFacade() {
         return recruiterFacade(new InMemoryRecruiterRepository());
     }
