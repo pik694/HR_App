bb7e5db7bf774e1c928c2527f7bdbf4123f4e679
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-06-01 00:43:18 +0200

    CI changes

--- docker-compose/compose_production.sh
+++ docker-compose/compose_production.sh
@@ -3,4 +3,4 @@
 cp ../recruit-me-app/target/recruit-me-app*.jar ./backend/app.jar
 cp -r ../recruit-me-web/dist/ ./frontend/files/
 
-docker-compose -f docker-compose.yaml -f docker-compose.prod.yaml -p prod up -d
+docker-compose -f docker-compose.yaml -f docker-compose.prod.yaml -p prod up -d --build
--- docker-compose/compose_rc.sh
+++ docker-compose/compose_rc.sh
@@ -4,4 +4,4 @@
 cp ../recruit-me-app/target/recruit-me-app*.jar ./backend/app.jar
 cp -r ../recruit-me-web/dist/ ./frontend/files/
 
-docker-compose -f docker-compose.yaml -f docker-compose.rc.yaml -p rc up -d
+docker-compose -f docker-compose.yaml -f docker-compose.rc.yaml -p rc up -d --build
