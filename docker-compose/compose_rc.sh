309c60733d44cc6be97b6df1ad85b55fee950c85
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-05-31 23:48:08 +0200

    Add docker composer configuration

---
+++ docker-compose/compose_rc.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+cp ../recruit-me-app/target/recruit-me-app*.jar ./backend/app.jar
+cp -r ../recruit-me-web/dist/ ./frontend/files/
+
+docker-compose -f docker-compose.yaml -f docker-compose.rc.yaml -p rc up -d
