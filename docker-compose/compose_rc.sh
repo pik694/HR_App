9d406ea1ded13a34cc50e55617c7778edc2e32b6
Daniel Bigos <daniel.bigos@icloud.com>
2020-04-01 22:25:22 +0200

    chore: remove frontend

--- docker-compose/compose_rc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-cp ../recruit-me-app/target/recruit-me-app*.jar ./backend/app.jar
-cp -r ../recruit-me-web/dist/ ./frontend/files/
-
-docker-compose -p rc down --rmi local
-docker-compose -f docker-compose.yaml -f docker-compose.rc.yaml -p rc up -d --build
